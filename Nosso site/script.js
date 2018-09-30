12ade5e0aacfbbbe3cb25779d95d3f53dc29a0d1
LiliLB <43713263+LiliLB@users.noreply.github.com>
2018-09-30 16:16:30 -0300

    Add files via upload

---
+++ Nosso site/script.js
@@ -0,0 +1,6 @@
+let nome= prompt ("Bem vindo ao Muquiranas qual é o seu nome?");
+let idade= prompt("Qual a sua idade?");
+
+if(idade>=18 && idade<=80){
+    alert("Parabéns " +nome+ ", você está dando o primeiro passo para iniciar uma aplicação financeira!");
+}
